4ddec6216560ac0dea5fced768d8c43b8d018930
miki-matsumoto <mikimatsumoto829@gmail.com>
2018-02-03 17:29:25 +0900

    heroku setup

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import http from 'http';
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(4646, '127.0.0.1');
+}).listen(process.env.PORT || 4646);
 
 console.log('http://localhost:4646')
