b4dafad13443c156720ce74373f934f50100bb95
miki-matsumoto <mikimatsumoto829@gmail.com>
2018-02-05 18:30:38 +0900

    WIP

--- crawring.js
+++ crawring.js
@@ -4,7 +4,6 @@ require('events').EventEmitter.defaultMaxListeners = 0
 
 // const BASE_URL = 'https://wovn.io'
 // const BASE_URL = 'http://nogizaka46.com'
-const BASE_URL = 'http://minimaltech.co/'
 
 const firstPageUrls = async () => {
   const browser = await puppeteer.launch({ headless: false })
@@ -32,26 +31,21 @@ const allPagesUrls = async () => {
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
   // const HOST = 'www.nogizaka46.com'
-  const HOST = 'minimaltech.co'
 
   var urls = await firstPageUrls()
   for (let i = 0; i < urls.length; i++) {
-    try {
-      await page.goto(urls[i])
+    await page.goto(urls[i])
 
-      const nodeLists = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('a')).map(node => {
-          return { host: node.host, href: node.href}
-        })
+    const nodeLists = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('a')).map(node => {
+        return { host: node.host, href: node.href}
       })
-      nodeLists.forEach(node => {
-        if (node.host === HOST && !urls.includes(node.href)) {
-          urls.push(node.href)
-        }
-      })
-    } catch (err) {
-      reject(err)
-    }
+    })
+    nodeLists.forEach(node => {
+      if (node.host === HOST && !urls.includes(node.href)) {
+        urls.push(node.href)
+      }
+    })
   }
 
   await browser.close()
